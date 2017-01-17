6e6d88a4e79900438a99ae45cf34317293d2a423
Seonho Kim <sunho529@gmail.com>
2017-01-17 11:05:53 +0900

    Init commit

---
+++ MeetingRooms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeetingRooms
+//
+//  Created by Seonho Kim on 2017. 1. 17..
+//  Copyright © 2017년 Seonho Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
